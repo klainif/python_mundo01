fc61129fba4471863ab74ddc4a62d83aeaf55d36
GlacioKlain <>
2022-03-21 15:59:52 +0000

    Initial commit

---
+++ funcoes.py
@@ -0,0 +1,13 @@
+def olamundo():
+  print('Olá, mundo!')
+
+
+def boasvindas(nome):
+  print(f'Seja bem-vindo, {nome}!')
+
+
+def soma(a, b):
+  s = a + b
+  print(f'A soma entre {a} e {b} é: {s}')
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import funcoes
+
+
+#Desafio 001 - Deixando tudo pronto.
+#Crie um programa que escreva “Olá, mundo!” na tela.
+
+
+funcoes.olamundo()
+
+
+#Desafio 002 - Respondendo ao Usuário.
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+
+nome = input('Digite seu nome, por favor: ')
+funcoes.boasvindas(nome)
+
+
+#Desafio 003 - Somando dois números.
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+
+a = float(input('Digite um número, por favor: '))
+b = float(input('Digite outro número, por favor: '))
+funcoes.soma(a, b)
+
+
+#Desafio 004 - Dissecando uma variável.
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+
